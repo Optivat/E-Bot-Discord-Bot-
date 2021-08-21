cea4ab111e18174d86b40e907bcdf76fcab5c3a5
Optivat <67515969+Optivat@users.noreply.github.com>
2021-08-20 23:30:42 -0700

    Added checks for changing nickname

--- src/main/java/events/NicknameChangingSystem.java
+++ src/main/java/events/NicknameChangingSystem.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import essentials.Main;
 import essentials.Methods;
 import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -12,6 +13,8 @@ public class NicknameChangingSystem extends ListenerAdapter {
 	
 	Main main = new Main();
 
+	String[] profanitylist = {"fuck","bitch","shit","sex","nigga","ass","slut","nigger"};
+	
 	public void onGuildMessageReceived(GuildMessageReceivedEvent e) {
 	String[] args = e.getMessage().getContentRaw().split("\\s+");
 	if(args[0].equalsIgnoreCase(main.prefix + "togglenicknamesystem") && e.getAuthor().getId().equalsIgnoreCase("263066454004465665")) {
@@ -37,13 +40,49 @@ public class NicknameChangingSystem extends ListenerAdapter {
 	if(Main.prop.get("nicknamechannel") != null && e.getChannel().getId().equalsIgnoreCase(Main.prop.getProperty("nicknamechannel"))) {
 		e.getMessage().delete().queue();
 		String nickname = e.getMessage().toString().trim();
-		e.getMember().getUser().openPrivateChannel().queue(channel -> {
-			EmbedBuilder eb2 = new EmbedBuilder();
-			eb2.setTitle("Nickname Change", null);
-			eb2.setColor(Color.red);
-			eb2.setDescription("You've changed your nickname from " + e.getMember().getNickname() + " to " + nickname);
-			channel.sendMessageEmbeds(eb2.build()).queue();
-		});
+		if(e.getMember().getPermissions().contains(Permission.ADMINISTRATOR)) {
+			e.getMember().getUser().openPrivateChannel().queue(channel -> {
+				EmbedBuilder eb2 = new EmbedBuilder();
+				eb2.setTitle("Nickname Change Error!", null);
+				eb2.setColor(Color.red);
+				eb2.setDescription("Unable to change nickname from " + e.getMember().getNickname() + " to " + nickname + "due to not being able to modify you, perhaps you have a superior role than the bot?");
+				channel.sendMessageEmbeds(eb2.build()).queue();
+			});
+		} else {
+			boolean inappropiatenickame = false;
+			for(int x=0; x<profanitylist.length - 1; x++) {
+				if(nickname.contains(profanitylist[x])) {
+					inappropiatenickame = true;
+				}
+			}
+			if(inappropiatenickame == true) {
+				e.getMember().getUser().openPrivateChannel().queue(channel -> {
+					EmbedBuilder eb2 = new EmbedBuilder();
+					eb2.setTitle("Nickname Change Error!", null);
+					eb2.setColor(Color.red);
+					eb2.setDescription("Unable to change your nickname from " + e.getMember().getNickname() + " to " + nickname + "due to containing a form of profanity. Please consider using a different name.");
+					channel.sendMessageEmbeds(eb2.build()).queue();
+				});
+			} else {
+				if(nickname.length() >= 32)  {
+					e.getMember().getUser().openPrivateChannel().queue(channel -> {
+						EmbedBuilder eb2 = new EmbedBuilder();
+						eb2.setTitle("Nickname Change Error!", null);
+						eb2.setColor(Color.red);
+						eb2.setDescription("Unable to change your nickname from " + e.getMember().getNickname() + " to " + nickname + "due to it being more than 32 characters, please decrease it.");
+						channel.sendMessageEmbeds(eb2.build()).queue();
+					});
+				} else {
+					e.getMember().getUser().openPrivateChannel().queue(channel -> {
+						EmbedBuilder eb2 = new EmbedBuilder();
+						eb2.setTitle("Nickname Change", null);
+						eb2.setColor(Color.red);
+						eb2.setDescription("You've changed your nickname from " + e.getMember().getNickname() + " to " + nickname);
+						channel.sendMessageEmbeds(eb2.build()).queue();
+					});	
+				}
+			}	
+		}
 	}
 	
 	}
